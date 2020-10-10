3197bc61c0d8ddee55f953e8e33aac497182e891
Eduardo0725 <eduardom0142@gmail.com>
2020-10-10 18:52:14 -0300

    Carousel atualizado

--- laravel/public/js/carousel.js
+++ laravel/public/js/carousel.js
@@ -1,7 +1,7 @@
 for (let index = 0; index <= 1; index++) {
     let side = (index == 0) ? 'left' : 'right';
-    document.querySelectorAll('label.' + side).forEach(label => {
-        let result = label.addEventListener('click', e => {
+    document.querySelectorAll('label.' + side).forEach(label => 
+        label.addEventListener('click', e => {
             let doc = document.querySelector('div.carousel#' + e.path[2].id + ' span.contents');
             let num = (parseInt(doc.getAttribute('number')) + ((side == 'left') ? 240 : -240));
 
@@ -10,11 +10,8 @@ for (let index = 0; index <= 1; index++) {
             doc.removeAttribute('number');
             doc.setAttribute('number', num);
 
-            console.log(doc.getAttribute('number'));
-            console.log('teste: ' + e.path[2].id);
-
             document.querySelector('label.left.' + e.path[2].id).style = (num == 0) ? '' : 'display: block;';
             document.querySelector('label.right.' + e.path[2].id).style = (num > -2880) ? '' : 'display: none;';
         })
-    });
+    );
 }
